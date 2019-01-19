cdc6ecbc37b8728ee4bdceb3bb89d235bd897b4a
brandonpacheco09 <brandonpacheco24@gmail.com>
2019-01-19 02:22:38 -0700

    pull from db/creating dashboard

--- data_visualization/src/components/Senddata.js
+++ data_visualization/src/components/Senddata.js
@@ -1,26 +1,55 @@
 import React, {Component} from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import 'react-widgets/dist/css/react-widgets.css';
+import DropdownList from 'react-widgets/lib/DropdownList';
+import Widget from './Widget';
 
 class Senddata extends Component {
   constructor(props){
     super(props);
     this.state = {
-      message: ''
+      data:[]
     };
-    console.log(props);
-    const dab = this.props.db.database().ref('/messages');
-    dab.push({
-      message: 'suck it'
-    });
+    this.refreshHandler = this.refreshHandler.bind(this);
+    //console.log(props);
   }
 
+  refreshHandler(){
+    console.log(this.state.data);
+  }
   componentDidMount(){
     const app = this.props.db.database().ref('/messages');
-    app.on('value', snapshot => {console.log(snapshot.val())});
+    app.on('value', snapshot => {
+      var list = snapshot.val();
+      var newList = []
+      //console.log(list);
+      for (let item in list){
+        newList.push({
+          src: list[item].message.src,
+          dest: list[item].message.dest,
+          Eth: list[item].message.Eth
+        });
+      }
+      //console.log(newList);
+      this.setState({data: newList});
+    });
+
+    //console.log(this.state.date);
   }
   render(){
+
     return (
       <div>
         MessageList Component
+        <Button style={{display: 'block'}} bsSize="large" onClick={this.refreshHandler}>
+          Print Data
+        </Button >
+        <Widget/>
+        <Widget/>
+        <Widget/>
+        <Widget/>
+        <Widget/>
+        <Widget/>
       </div>
     )
   }
---
+++ data_visualization/src/components/Widget.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './css/Widget.css';
+
+class Widget extends Component{
+
+  render(){
+    return(
+      <div class='outershell'>
+      Hello
+      </div>
+    );
+  }
+}
+
+export default Widget;
