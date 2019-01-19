07c8fa647149410ff67de09c14357210a3747500
bull313 <=>
2019-01-19 11:57:16 -0700

    Fixing bugs/merge conflicts
    Issue #10

--- data_visualization/src/components/Widget.js
+++ data_visualization/src/components/Widget.js
@@ -2,26 +2,16 @@ import React, {Component} from 'react';
 import './css/Widget.css';
 import {Bar} from 'react-chartjs-2';
 
-/* Class Widget is going to be the boxed areas for each graph.*/
 class Widget extends Component{
 
-//Need to call super(props) to get values passed in from Senddata.js
   constructor(props){
     super(props);
   }
-<<<<<<< HEAD
-  render(){
-    //using a temporary empty object for the charts data.
-    var dat = {}
-    //checking to see what type of graph we need to execute.
-    if(this.props.type == 'Bar'){
-=======
 
   render() {
     var dat = {};
 
     if(this.props.type == 'Bar') {
->>>>>>> f194d082bc72566791fd66941f476ba074139790
        dat = {
         chartData: {
           labels: this.props.labels,
@@ -38,13 +28,8 @@ class Widget extends Component{
         }
       }
     }
-<<<<<<< HEAD
-    
-    return(
-=======
 
     return (
->>>>>>> f194d082bc72566791fd66941f476ba074139790
       <div class='outershell'>
         {this.props.type == 'Bar' &&
         <Bar data={dat.chartData} options={{
